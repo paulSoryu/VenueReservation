e81fdab8969d326c761896acd26f92c12dac707b
paulSoryu <paul.soryu@gmail.com>
2026-09-01 15:27:42 +0300

    Delete all unnecessary for API-only solution files and folders

--- VenueReservation/Controllers/HomeController.cs
+++ VenueReservation/Controllers/HomeController.cs
@@ -1,25 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using VenueReservation.Models;
 
 namespace VenueReservation.Controllers
 {
-    public class HomeController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Get()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Ok(new { message = "API is working", status = "Success" });
         }
     }
 }
